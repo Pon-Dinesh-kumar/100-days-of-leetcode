1f704b5aafbc66a3d8d62baf2d4a8fdfca556765
M.Pon Dinesh Kumar <126751667+Pon-Dinesh-kumar@users.noreply.github.com>
2023-08-29 16:57:06 +0000

    Daily LeetCode submission Runtime - 1 ms (23.85%), Memory - 41.9 MB (83.07%)

---
+++ 100daysofcode/0031-next-permutation/solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    public void nextPermutation(int[] nums) {
+        int len = nums.length;
+        int i = len - 2;
+        while (i >= 0 && nums[i] >= nums[i + 1]) {
+            i--;
+        }
+        if (i >= 0) {
+            int j = len - 1;
+            while (nums[j] <= nums[i]) {
+                j--;
+            }
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+        }
+        Arrays.sort(nums, i + 1, len);
+    }
+}
+
