15643b3d42edf930ab2183a108f7814cba4845a6
M.Pon Dinesh Kumar <126751667+Pon-Dinesh-kumar@users.noreply.github.com>
2023-09-19 15:55:36 +0000

    Daily LeetCode submission Runtime - 5 ms (51.94%), Memory - 39.4 MB (79.80%)

---
+++ 100daysofcode/0441-arranging-coins/solution.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int arrangeCoins(int n) {
+        int count = 0;
+        for(int i = 1; i<=n; i++){
+            count++;
+            n = n-i;
+        }
+        return count;
+    }
+}
