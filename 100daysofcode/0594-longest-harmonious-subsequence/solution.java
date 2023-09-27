1723467ecfc7456a8e7aaadd7383630f5b511eec
M.Pon Dinesh Kumar <126751667+Pon-Dinesh-kumar@users.noreply.github.com>
2023-09-27 17:39:28 +0000

    Daily LeetCode submission Runtime - 12 ms (100.00%), Memory - 43.9 MB (89.53%)

---
+++ 100daysofcode/0594-longest-harmonious-subsequence/solution.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int findLHS(int[] nums) {
+        if(nums.length==0){
+            return 0;
+        }
+        if(nums.length==1){
+            return 0;
+        }
+        Arrays.sort(nums);
+        int maxCount = 0;
+        int currentCount = 1;
+        int prevCount = 0;
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == nums[i - 1]) {
+                currentCount++;
+            } else if (nums[i] - nums[i - 1] == 1) {
+                prevCount = currentCount;
+                currentCount = 1;
+            } else {
+                prevCount = 0;
+                currentCount = 1;
+            }
+
+            if (prevCount > 0) {
+                maxCount = Math.max(maxCount, prevCount + currentCount);
+            }
+        }
+
+        return maxCount;
+    }
+}
+
