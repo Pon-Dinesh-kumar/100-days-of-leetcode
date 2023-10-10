5a13393675cfdf5237c8a52a6e2d630a369886b9
M.Pon Dinesh Kumar <126751667+Pon-Dinesh-kumar@users.noreply.github.com>
2023-10-10 14:34:13 +0000

    Daily LeetCode submission Runtime - 3 ms (56.62%), Memory - 40.4 MB (79.95%)

---
+++ 100daysofcode/1960-check-if-the-sentence-is-pangram/solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean checkIfPangram(String sentence) {
+        HashSet<Character> set = new HashSet<>();
+
+        for (int i = 0; i < sentence.length(); i++) {
+            char c = sentence.charAt(i);
+                set.add(c);
+            
+
+            if (set.size() == 26) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
+
