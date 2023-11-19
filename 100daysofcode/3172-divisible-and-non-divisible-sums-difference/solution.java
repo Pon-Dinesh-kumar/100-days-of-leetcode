0556e904f14e58df8850585335edde490faabacf
M.Pon Dinesh Kumar <126751667+Pon-Dinesh-kumar@users.noreply.github.com>
2023-11-19 16:36:25 +0000

    Daily LeetCode submission Runtime - 1 ms (93.52%), Memory - 40.4 MB (6.32%)

---
+++ 100daysofcode/3172-divisible-and-non-divisible-sums-difference/solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int differenceOfSums(int n, int m) {
+
+        int x=0;
+        int y =0;
+
+        for(int i=1; i<=n; i++){
+            if(i%m!=0){
+                x+=i;
+            }
+            else
+            {
+                y+=i;
+            }
+        }
+
+        return x-y;
+        
+    }
+}
