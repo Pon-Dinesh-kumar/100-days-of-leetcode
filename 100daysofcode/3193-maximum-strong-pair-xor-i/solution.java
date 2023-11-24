a4f6cffd2429f099670868f5f84ec6d72bd503c9
M.Pon Dinesh Kumar <126751667+Pon-Dinesh-kumar@users.noreply.github.com>
2023-11-24 14:48:07 +0000

    Daily LeetCode submission Runtime - 3 ms (65.19%), Memory - 43.2 MB (36.79%)

---
+++ 100daysofcode/3193-maximum-strong-pair-xor-i/solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maximumStrongPairXor(int[] nums) {
+
+        int ans = 0;
+
+        for(int i=0;i<nums.length;i++){
+            for(int j=0;j<nums.length;j++){
+                if(Math.abs(nums[i]-nums[j])<=Math.min(nums[i], nums[j])){
+                    int temp = nums[i]^nums[j];
+                    ans=Math.max(temp, ans);
+                }
+            }
+        }
+        return ans;
+    }
+}
