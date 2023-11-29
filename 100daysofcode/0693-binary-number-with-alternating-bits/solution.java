b25b25fdc140bdc68fea5c16304b543e648321a2
M.Pon Dinesh Kumar <126751667+Pon-Dinesh-kumar@users.noreply.github.com>
2023-11-29 17:50:56 +0000

    Daily LeetCode submission Runtime - 0 ms (100.00%), Memory - 39.5 MB (37.57%)

---
+++ 100daysofcode/0693-binary-number-with-alternating-bits/solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean hasAlternatingBits(int n) {
+
+        while(n!=0){
+            int temp1 = n&1;
+            int temp2 = (n>>1)&1;
+            if(temp1==temp2){
+                return false;
+            }
+            n = n>>1;
+        }
+        return true;
+        
+    }
+}
