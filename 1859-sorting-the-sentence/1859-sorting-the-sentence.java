7179eecd4713208c03b9960cefae869af5201c12
M.Pon Dinesh Kumar <126751667+Pon-Dinesh-kumar@users.noreply.github.com>
2023-12-09 08:56:06 +0530

    Time: 0 ms (100.00%), Space: 40.6 MB (37.17%) - LeetHub

---
+++ 1859-sorting-the-sentence/1859-sorting-the-sentence.java
@@ -0,0 +1,27 @@
+class Solution {
+    public String sortSentence(String s) {
+        String[] arr = new String[58];
+        StringBuilder str = new StringBuilder();
+        for(int i=0; i<s.length();i++){
+            char ch = s.charAt(i);
+            if(ch>=48 && ch<=57){
+                arr[(int)(ch)] = str.toString();
+                str.setLength(0);
+                i++;
+            }
+            else{
+                str.append(ch);
+            }
+        }
+
+        str.setLength(0);
+        for(int i=48; i<=57; i++){
+            if(arr[i] != null && !arr[i].isEmpty()){
+                str.insert(str.length(),arr[i]);
+                str.append(' ');
+            }
+        }
+        return str.toString().trim();
+        
+    }
+}
