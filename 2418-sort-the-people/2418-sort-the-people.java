c5100e824f19831681c1fe029a83221b2831d87c
M.Pon Dinesh Kumar <126751667+Pon-Dinesh-kumar@users.noreply.github.com>
2023-12-18 23:05:25 +0530

    Time: 22 ms (28.09%), Space: 44.5 MB (48.74%) - LeetHub

---
+++ 2418-sort-the-people/2418-sort-the-people.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String[] sortPeople(String[] names, int[] heights) {
+
+        for(int i =0; i<heights.length; i++){
+            for(int j=i+1; j<heights.length; j++){
+                if(heights[i]<heights[j]){
+                    int temp=heights[i];
+                    heights[i]=heights[j];
+                    heights[j]=temp;
+
+                    String temp2=names[i];
+                    names[i]=names[j];
+                    names[j]=temp2;
+                }
+            }
+        }
+        return names;  
+    }
+}
